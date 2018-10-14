7d23bd4a6b453230ac98393fc68c7655f68b6d05
Christian <christianrdumas11@gmail.com>
2018-10-14 16:26:23 -0400

    Add extra characters and automatic word wrap

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
 import png
+import textwrap
 
 green_rgb = [63, 101, 5]
 
@@ -22,6 +23,14 @@ for a in range(len(bit_list)):
 bit_dict[':'] = '000010000010000'
 bit_dict['3'] = '110001010001110'
 bit_dict['<'] = '001010100010001'
+bit_dict['1'] = '010110010010111'
+bit_dict['2'] = '110001010100111'
+bit_dict['4'] = '101101111001001'
+bit_dict['5'] = '111100010001110'
+bit_dict['6'] = '011100110101010'
+bit_dict['7'] = '111001010100100'
+bit_dict['8'] = '010101010101010'
+bit_dict['9'] = '010101011001110'
 
 def print_char(array, x, y, char_to_print, scale):
     for a in range(15):
@@ -36,13 +45,15 @@ def print_char(array, x, y, char_to_print, scale):
 
  
 def produce_image(input_text):
-    max_width = 12
-    max_height = 3
+    max_width = 15
+    max_height = 4
     max_len = max_width * max_height
-    input_text = "".join(x for x in input_text if x.isalpha() or x == ' ')
+    input_text = "".join(x for x in input_text if x.isalpha() or x == ' ' or x.isnumeric())
     input_text = input_text.upper()
     input_text = input_text[:max_len - 5]
     input_text = ":33<" + input_text
+    input_text = textwrap.wrap(input_text, max_width)
+    input_text = input_text[:max_height]
     r=png.Reader(filename='/home/pi/Documents/img.png')
 
     r1 = r.read(lenient=True)
@@ -55,12 +66,14 @@ def produce_image(input_text):
     x_delta = 0
     y_delta = 0
 
-    scale = 5
-    for a in range(len(input_text)):
-        x_delta = (a%max_width)*scale*4
-        y_delta = int(a/max_width)*scale*10
-        if input_text[a] != ' ':
-            l = print_char(l, left_bound+x_delta, top_bound+y_delta, input_text[a], scale)
+    scale = 4
+    for a in input_text:
+        for b in range(len(a)):
+            if a[b] != ' ':
+                l = print_char(l, left_bound+x_delta, top_bound+y_delta, a[b], scale)
+            x_delta += scale*4
+        x_delta = 0
+        y_delta += scale*10
             
 
     png.Writer( r1[0], r1[1] ).write(open('/home/pi/Documents/out_img.png', 'wb+'), l)
