aeba19d9e97acafd3aa0aa68cc65cab285d0aaae
Christian <christianrdumas11@gmail.com>
2018-10-15 00:14:29 -0400

    Added ROI JSON and quick-add for new characters

--- temp.py
+++ temp.py
@@ -1,7 +1,10 @@
 import png
 import textwrap
+import json
 
 green_rgb = [63, 101, 5]
+
+troll_colors = {'nepeta':[63, 101, 5],'vriska':[0, 64, 128],'karkat':[108, 108, 108]}
 
        
 conv_list = []
@@ -32,48 +35,52 @@ bit_dict['7'] = '111001010100100'
 bit_dict['8'] = '010101010101010'
 bit_dict['9'] = '010101011001110'
 
-def print_char(array, x, y, char_to_print, scale):
+def print_char(array, x, y, char_to_print, scale, color):
     for a in range(15):
         xt = (a % 3)*scale
         yt = int(a / 3)*scale
         if bit_dict[char_to_print][a] == '1':
             for val in range(scale*scale):
-                array[y+yt+int(val/scale)][(x+xt+(val%scale))*3] = green_rgb[0]
-                array[y+yt+int(val/scale)][(x+xt+(val%scale))*3+1] = green_rgb[1]
-                array[y+yt+int(val/scale)][(x+xt+(val%scale))*3+2] = green_rgb[2]
+                array[y+yt+int(val/scale)][(x+xt+(val%scale))*3] = color[0]
+                array[y+yt+int(val/scale)][(x+xt+(val%scale))*3+1] = color[1]
+                array[y+yt+int(val/scale)][(x+xt+(val%scale))*3+2] = color[2]
     return array
 
  
-def produce_image(input_text):
-    max_width = 15
-    max_height = 4
+def produce_image(input_text, character):
+    character = json.loads(open(character+'.json').read())
+    scale = 4
+    char_width = scale * 4
+    char_height = scale * 10
+    max_width = int((character['right']-character['left'])/char_width)
+    max_height = int((character['bottom']-character['top'])/char_height)
     max_len = max_width * max_height
     input_text = "".join(x for x in input_text if x.isalpha() or x == ' ' or x.isnumeric())
     input_text = input_text.upper()
-    input_text = input_text[:max_len - 5]
-    input_text = ":33<" + input_text
+    if character['name'] == 'nepeta':
+        input_text = ":33<" + input_text
+    input_text = input_text[:max_len]
     input_text = textwrap.wrap(input_text, max_width)
     input_text = input_text[:max_height]
-    r=png.Reader(filename='/home/pi/Documents/img.png')
+    r=png.Reader(filename='/home/pi/Documents/' + character['name'] + '.png')
 
     r1 = r.read(lenient=True)
 
     l=list(r.read()[2])
 
-    left_bound = 300
-    top_bound = 165
+    left_bound = character['left']
+    top_bound = character['top']
 
     x_delta = 0
     y_delta = 0
 
-    scale = 4
     for a in input_text:
         for b in range(len(a)):
             if a[b] != ' ':
-                l = print_char(l, left_bound+x_delta, top_bound+y_delta, a[b], scale)
-            x_delta += scale*4
+                l = print_char(l, left_bound+x_delta, top_bound+y_delta, a[b], scale, troll_colors[character['name']])
+            x_delta += char_width
         x_delta = 0
-        y_delta += scale*10
+        y_delta += char_height
             
 
     png.Writer( r1[0], r1[1] ).write(open('/home/pi/Documents/out_img.png', 'wb+'), l)
